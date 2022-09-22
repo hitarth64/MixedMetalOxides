d61b603a86ab720e4729af2bca47a73204c77355
Hitarth Choubisa <hitarthc64@gmail.com>
2022-09-22 12:25:52 -0400

    Create data.py

---
+++ mmo/data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+class Data:
+  def __init__(self):
+    self.path = 'datasets/'
+    
+  def GetData(self, type):
+    if type == 'surface':
+      return pd.read_pickle(self.path+type+'.pkl')
+    elif type == 'o':
+      return pd.read_pickle(self.path+type+'Rad.pkl')
+    elif type == 'oh':
+      return pd.read_pickle(self.path+type+'Rad.pkl')
+    elif type == 'ooh':
+      return pd.read_pickle(self.path+type+'Rad.pkl')
+    
